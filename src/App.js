7dafd218794635e9af9f685a7870822e104602cc
irfanfath <mirfanf7@gmail.com>
2024-01-29 19:12:16 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import FaceLandmarker from './component/FaceLandmarkerDemo';
-// import TestFace from '../src/component/TestFace';
 import FaceTest from './component/FaceTest';
 
 function App() {
   return (
     <div className="App">
-      {/* <FaceTest /> */}
-      <FaceLandmarker />
+      <FaceTest />
+      {/* <FaceLandmarker /> */}
     </div>
   );
 }
