0047a0172d35c2b06f3bbbea5ca894f8ed095059
irfanfath <mirfanf7@gmail.com>
2024-01-29 18:18:47 +0700

    update

--- src/component/FaceLandmarkerDemo.jsx
+++ src/component/FaceLandmarkerDemo.jsx
@@ -28,7 +28,7 @@ const FaceLandmarker = () => {
       );
       const newFaceLandmarker = await vision.FaceLandmarker.createFromOptions(filesetResolver, {
         baseOptions: {
-          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+          modelAssetPath: './lib/face_landmarker.task',
           delegate: "GPU"
         },
         outputFaceBlendshapes: true,
