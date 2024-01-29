23d758007dd147ea2eb815f04f92eeb0366d4664
irfanfath <mirfanf7@gmail.com>
2024-01-29 20:29:15 +0700

    Update FaceLandmarkerDemo.jsx

--- src/component/FaceLandmarkerDemo.jsx
+++ src/component/FaceLandmarkerDemo.jsx
@@ -12,23 +12,15 @@ const FaceLandmarker = () => {
   const webcamRunningRef = useRef(false);
   const videoBlendShapesRef = useRef(null);
   const videoWidth = 480;
-  const [pipelineIndex, setPipelineIndex] = useState(0);
-  const [capturedImage, setCapturedImage] = useState('');
-
-  const pipeline = [
-    { task: 'hadap-kiri' },
-    { task: 'hadap-kanan' },
-    { task: 'buka-mulut' }
-  ];
-
+  
   useEffect(() => {
     const createFaceLandmarker = async () => {
       const filesetResolver = await vision.FilesetResolver.forVisionTasks(
-        process.env.PUBLIC_URL + "/wasm"
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
       );
       const newFaceLandmarker = await vision.FaceLandmarker.createFromOptions(filesetResolver, {
         baseOptions: {
-          modelAssetPath: './lib/face_landmarker.task',
+          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
           delegate: "GPU"
         },
         outputFaceBlendshapes: true,
@@ -37,8 +29,90 @@ const FaceLandmarker = () => {
       });
       setFaceLandmarker(newFaceLandmarker);
     };
+
     createFaceLandmarker();
   }, []);
+
+  const handleClick = async (event) => {
+    if (!faceLandmarker) {
+      console.log("Wait for faceLandmarker to load before clicking!");
+      return;
+    }
+
+    if (runningMode === "VIDEO") {
+      setRunningMode("IMAGE");
+      await faceLandmarker.setOptions({ runningMode });
+    }
+
+    const allCanvas = event.target.parentNode.getElementsByClassName("canvas");
+    for (let i = allCanvas.length - 1; i >= 0; i--) {
+      const n = allCanvas[i];
+      n.parentNode.removeChild(n);
+    }
+
+    const faceLandmarkerResult = await faceLandmarker.detect(event.target);
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("class", "canvas");
+    canvas.setAttribute("width", event.target.naturalWidth + "px");
+    canvas.setAttribute("height", event.target.naturalHeight + "px");
+    canvas.style.left = "0px";
+    canvas.style.top = "0px";
+    canvas.style.width = `${event.target.width}px`;
+    canvas.style.height = `${event.target.height}px`;
+
+    event.target.parentNode.appendChild(canvas);
+    const ctx = canvas.getContext("2d");
+    const drawingUtils = new vision.DrawingUtils(ctx);
+    for (const landmarks of faceLandmarkerResult.faceLandmarks) {
+      drawingUtils.drawConnectors(
+        landmarks,
+        vision.FaceLandmarker.FACE_LANDMARKS_TESSELATION,
+        { color: "#C0C0C070", lineWidth: 1 }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        vision.FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
+        { color: "#FF3030" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        vision.FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
+        { color: "#FF3030" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        vision.FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
+        { color: "#30FF30" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        vision.FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
+        { color: "#30FF30" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        vision.FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
+        { color: "#E0E0E0" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        vision.FaceLandmarker.FACE_LANDMARKS_LIPS,
+        { color: "#E0E0E0" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        vision.FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
+        { color: "#FF3030" }
+      );
+      drawingUtils.drawConnectors(
+        landmarks,
+        vision.FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
+        { color: "#30FF30" }
+      );
+    }
+
+    drawBlendShapes(videoBlendShapesRef.current, faceLandmarkerResult.faceBlendshapes);
+  };
 
   const enableCam = async () => {
     if (!faceLandmarker) {
@@ -64,6 +138,14 @@ const FaceLandmarker = () => {
   };
 
   const predictWebcam = async () => {
+    const radio = videoRef.current.videoHeight / videoRef.current.videoWidth;
+    videoRef.current.style.width = videoWidth + "px";
+    videoRef.current.style.height = videoWidth * radio + "px";
+    canvasRef.current.style.width = videoWidth + "px";
+    canvasRef.current.style.height = videoWidth * radio + "px";
+    canvasRef.current.width = videoRef.current.videoWidth;
+    canvasRef.current.height = videoRef.current.videoHeight;
+
     if (runningMode === "IMAGE") {
       setRunningMode("VIDEO");
       await faceLandmarker.setOptions({ runningMode: "VIDEO" });
@@ -97,82 +179,50 @@ const FaceLandmarker = () => {
     if (!blendShapes || !blendShapes.length) {
       return;
     }
-
-    const eyelookinleftValue = blendShapes[0]?.categories.find(
-      (shape) => shape.categoryName === "eyeLookInLeft"
-    )?.score;
-
-    const eyelookinrightValue = blendShapes[0]?.categories.find(
-      (shape) => shape.categoryName === "eyeLookInRight"
-    )?.score;
-
-    const jawopenValue = blendShapes[0]?.categories.find(
-      (shape) => shape.categoryName === "jawOpen"
-    )?.score;
-
-    const currentTask = pipeline[pipelineIndex]?.task;
-    const newIndex = (pipelineIndex + 1)
-
-    // If eyelookinleft value is greater than 0.7 and the current task is 'hadap-kiri', move to the next item in the pipeline
-    if (eyelookinleftValue > 0.7 && currentTask === 'hadap-kiri') {
-      if (newIndex !== pipeline.length) {
-        setPipelineIndex(pipelineIndex + 1)
-      } else {
-        captureImage();
-      }
-    } else if (eyelookinrightValue > 0.7 && currentTask === 'hadap-kanan') {
-      if (newIndex !== pipeline.length) {
-        setPipelineIndex(pipelineIndex + 1)
-      } else {
-        captureImage();
-      }
-    } else if (jawopenValue > 0.7 && currentTask === 'buka-mulut') {
-      if (newIndex !== pipeline.length) {
-        setPipelineIndex(pipelineIndex + 1)
-      } else {
-        captureImage();
-      }
-    }
+    
+    // const eyelookinleftValue = blendShapes[0].categories.find(
+    //   (shape) => shape.categoryName === "eyeLookInLeft"
+    // )?.score;
+  
+    // if (eyelookinleftValue !== undefined) {
+    //   console.log("eyelookinleft:", eyelookinleftValue);
+    // }
+  
+    let htmlMaker = "";
+  
+    (blendShapes?.[0]?.categories ?? []).forEach((shape) => {
+      htmlMaker += `
+        <li class="blend-shapes-item">
+          <span class="blend-shapes-label">${shape.displayName || shape.categoryName}</span>
+          <span class="blend-shapes-value" style="width: calc(${+shape.score * 100}% - 120px)">
+            ${Math.round(parseFloat(+shape.score) * 100) + "%"}
+          </span>
+        </li>
+      `;
+    });
+    el.innerHTML = htmlMaker;
   };
-
-  const captureImage = () => {
-    // Code to capture the image
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
-    canvas.width = videoRef.current.videoWidth;
-    canvas.height = videoRef.current.videoHeight;
-    ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-
-    // Convert the captured image to a base64 string
-    const imageData = canvas.toDataURL('image/jpeg');
-
-    // Set the captured image in the state
-    setCapturedImage(imageData);
-    enableCam();
-    console.log(imageData);
-  };
-
-
+  
   return (
     <div>
-      <section id="demos">
+      <section id="demos" className="invisible">
+        <h2>Demo: Webcam continuous face landmarks detection</h2>
+        <p>Hold your face in front of your webcam to get real-time face landmark detection. Click <b>enable webcam</b> below and grant access to the webcam if prompted.</p>
+
         <div id="liveView" className="videoView">
           <button id="webcamButton" onClick={enableCam} className="mdc-button mdc-button--raised">
+            <span className="mdc-button__ripple"></span>
             <span className="mdc-button__label">{webcamRunning ? 'DISABLE WEBCAM' : 'ENABLE WEBCAM'}</span>
           </button>
-          <div>{JSON.stringify(pipeline[pipelineIndex].task)}</div>
-          {capturedImage && (
-            <div>
-              <h2>Captured Image</h2>
-              <img src={capturedImage} alt="Captured" />
-            </div>
-          )}
           <div style={{ position: 'relative' }}>
             <video ref={videoRef} style={{ position: 'absolute', left: 0, top: 0 }} autoPlay playsInline></video>
             <canvas ref={canvasRef} className="output_canvas" style={{ position: 'absolute', left: 0, top: 0 }}></canvas>
           </div>
         </div>
 
+        <div className="blend-shapes">
+          <ul className="blend-shapes-list" ref={videoBlendShapesRef}></ul>
+        </div>
       </section>
     </div>
   );
