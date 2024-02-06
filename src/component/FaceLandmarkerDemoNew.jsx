3fdd03c6475fa06bf5772a4f0723bff91e0f3e9c
irfanfath <mirfanf7@gmail.com>
2024-02-07 00:47:03 +0700

    update UI

--- src/component/FaceLandmarkerDemoNew.jsx
+++ src/component/FaceLandmarkerDemoNew.jsx
@@ -274,20 +274,20 @@ const FaceLandmarker = () => {
           {dynamicPipelineRef.current[pipelineRef.current]?.task === 'hadap-kiri' ?
             <img className="bg-image" alt="" src={leftImage} />
             :
-            dynamicPipelineRef.current[pipelineRef.current]?.task === 'hadap-kanan' ? 
-            <img className="bg-image" alt="" src={rightImage} />
-            :
-            <img className="bg-image" alt="" src={frontImage} />
+            dynamicPipelineRef.current[pipelineRef.current]?.task === 'hadap-kanan' ?
+              <img className="bg-image" alt="" src={rightImage} />
+              :
+              <img className="bg-image" alt="" src={frontImage} />
           }
           <div style={{ position: 'relative' }}>
             <video ref={videoRef} style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100vh', objectFit: 'none', overflow: 'hidden' }} autoPlay playsInline></video>
             {loading ?
               <div style={{ position: 'fixed', fontSize: 26, fontWeight: 600, top: 50, left: 0, right: 0, zIndex: 1000 }}>
-                <span style={{ color: 'white' }}>Harap Tunggu<br /><span style={{ fontSize: 20 }}>sedang memproses kamera</span></span>
+                <span style={{ color: 'red' }}>Harap Tunggu<br /><span style={{ fontSize: 20 }}>sedang memproses kamera</span></span>
               </div>
               :
-              <div style={{ position: 'fixed', fontSize: 22, fontWeight: 600, top: 50, left: 0, right: 0, width: '100%', zIndex: 1000, backgroundColor: 'red', padding: '10px' }}>
-                <span style={{ color: 'white' }}>{(dynamicPipeline[pipelineIndex]?.word)}<br /><span style={{ fontSize: 20 }}>{instructionMessage}</span></span>
+              <div style={{ position: 'fixed', fontSize: 22, fontWeight: 600, top: 50, left: 0, right: 0, zIndex: 1000 }}>
+                <span style={{ color: 'red' }}>{(dynamicPipeline[pipelineIndex]?.word)}<br /><span style={{ fontSize: 20 }}>{instructionMessage}</span></span>
               </div>
             }
 
