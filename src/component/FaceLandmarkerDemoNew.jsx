4c67cc33b255b937ee910defc77d15685c348582
irfanfath <mirfanf7@gmail.com>
2024-07-10 15:47:05 +0700

    Update FaceLandmarkerDemoNew.jsx

--- src/component/FaceLandmarkerDemoNew.jsx
+++ src/component/FaceLandmarkerDemoNew.jsx
@@ -485,7 +485,7 @@ const FaceLandmarker = () => {
           }
           <div style={{ position: 'relative' }}>
             {videoRef &&
-              <video poster="noposter" ref={videoRef} style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100vh', objectFit: 'cover', overflow: 'hidden', transform: facingMode === 'user' ? 'scaleX(-1)' : 'none' }} autoPlay playsInline></video>
+              <video poster="noposter" ref={videoRef} style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100vh', objectFit: 'cover', overflow: 'hidden' }} autoPlay playsInline></video>
             }
             {
               rejectMessage !== '' ? (
