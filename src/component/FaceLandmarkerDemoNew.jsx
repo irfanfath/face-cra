90ca926aa8ebda09e364d2196180019a0593577c
irfanfath <mirfanf7@gmail.com>
2024-07-10 09:19:07 +0700

    Update FaceLandmarkerDemoNew.jsx

--- src/component/FaceLandmarkerDemoNew.jsx
+++ src/component/FaceLandmarkerDemoNew.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState, useRef, useCallback, useMemo } from 'react'
 import * as vision from '@mediapipe/tasks-vision';
 
 const pipeline = [
+  { task: 'ktp-extract', word: 'Silahkan Tunjukan KTP Anda' },
+  { task: 'face-liveness', word: 'Silahkan Lihat Kamera' },
   { task: 'hadap-kiri', word: 'Silahkan Hadap Kiri' },
   { task: 'hadap-kanan', word: 'Silahkan Hadap Kanan' },
   { task: 'buka-mulut', word: 'Silahkan Buka Mulut' },
@@ -29,15 +31,14 @@ const handleApi = async (body) => {
   }
 }
 
-
 const handleLiveness = (image) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ2Qk55YVpaU0dBVk5Zek12ZEp2ajhWUkdyOFVGUF9qUnh1dFdFd3Exa0RZIn0.eyJleHAiOi0xODAxNTkxNTU4LCJpYXQiOjE2MjkzNzU3MzgsImp0aSI6IjExYWVjZjJlLTNhNDMtNDEyMy05MDFjLTZkOGI0YjliMWMwOSIsImlzcyI6Imh0dHA6Ly9rZXljbG9hazo4MDgwL2F1dGgvcmVhbG1zL3BpY2Fzby1wbGF0Zm9ybSIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiIwYjU1OTNhMi03MTQ4LTRkNzAtOTBkMC0yMTI3NGQyMjdmMDEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJhZG1pbiIsInNlc3Npb25fc3RhdGUiOiI4OTRhYmE4OS1hYTFjLTQwNDEtYmIyZC0yNGQ2YTEwMDQ2NDAiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vZHNjLW9jci51ZGF0YS5pZDo4MDgzIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiYWJkYW5tdWxpYTQgYWJkYW5tdWxpYTQiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJjZDMyN2U3ZS1kNDQ0LTRkZGMtOTMyZS04NGYyYjBhOTMyY2EiLCJnaXZlbl9uYW1lIjoiYWJkYW5tdWxpYTQiLCJmYW1pbHlfbmFtZSI6ImFiZGFubXVsaWE0IiwiZW1haWwiOiJqc3Vwb3lvQGdtYWlsLmNvbSJ9.QHe4RwUVmRhE8DunHEte5DSgJfjfJ7MjDPkQUsOVNFUW600bAmAssAsWSCDNogUw__161jv6LzzBaqa0dTNEhZOmfl3wVoRK7Km1ZJsnSmcm6y2y05WbKKChvdbDTGw8zyCmt5iFOtnZLh1Y-U2M1EvogjzFTLHGf_FPPAHtGRXR9w2GOOiXjvCCLq9Nng7rtVyLj0vRAQG4KThkjm0mCIsWyUBnl96lmicARsedEhOH44DyrlyoXs5rA8BKbgXJuMKAorI36I3U-4C9IbBKfYQeZg0lo5Z-V4tbPVgNYvTnSK9lNCR3Su8polqTt8dFgg8QIIf-kv7bDtJ42EEJrA'    
+      const token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ2Qk55YVpaU0dBVk5Zek12ZEp2ajhWUkdyOFVGUF9qUnh1dFdFd3Exa0RZIn0.eyJleHAiOi0xODAxNTkxNTU4LCJpYXQiOjE2MjkzNzU3MzgsImp0aSI6IjExYWVjZjJlLTNhNDMtNDEyMy05MDFjLTZkOGI0YjliMWMwOSIsImlzcyI6Imh0dHA6Ly9rZXljbG9hazo4MDgwL2F1dGgvcmVhbG1zL3BpY2Fzby1wbGF0Zm9ybSIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiIwYjU1OTNhMi03MTQ4LTRkNzAtOTBkMC0yMTI3NGQyMjdmMDEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJhZG1pbiIsInNlc3Npb25fc3RhdGUiOiI4OTRhYmE4OS1hYTFjLTQwNDEtYmIyZC0yNGQ2YTEwMDQ2NDAiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vZHNjLW9jci51ZGF0YS5pZDo4MDgzIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiYWJkYW5tdWxpYTQgYWJkYW5tdWxpYTQiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJjZDMyN2U3ZS1kNDQ0LTRkZGMtOTMyZS04NGYyYjBhOTMyY2EiLCJnaXZlbl9uYW1lIjoiYWJkYW5tdWxpYTQiLCJmYW1pbHlfbmFtZSI6ImFiZGFubXVsaWE0IiwiZW1haWwiOiJqc3Vwb3lvQGdtYWlsLmNvbSJ9.QHe4RwUVmRhE8DunHEte5DSgJfjfJ7MjDPkQUsOVNFUW600bAmAssAsWSCDNogUw__161jv6LzzBaqa0dTNEhZOmfl3wVoRK7Km1ZJsnSmcm6y2y05WbKKChvdbDTGw8zyCmt5iFOtnZLh1Y-U2M1EvogjzFTLHGf_FPPAHtGRXR9w2GOOiXjvCCLq9Nng7rtVyLj0vRAQG4KThkjm0mCIsWyUBnl96lmicARsedEhOH44DyrlyoXs5rA8BKbgXJuMKAorI36I3U-4C9IbBKfYQeZg0lo5Z-V4tbPVgNYvTnSK9lNCR3Su8polqTt8dFgg8QIIf-kv7bDtJ42EEJrA'
       const blob = base64ToBlob(image, 'image/jpeg');
       const formData = new FormData();
       formData.append('image', blob, 'image.jpg');
-      
+
       const action = await fetch('https://bigvision.id/upload/face-liveness', {
         body: formData,
         method: "POST",
@@ -76,7 +77,7 @@ function base64ToBlob(base64, type = 'application/octet-stream') {
   return new Blob([bytes], { type });
 }
 
-const DEFAULT_VALUE_PIPELINE = [0, 1, 2, 3];
+const DEFAULT_VALUE_PIPELINE = [0, 1, 2, 3, 4, 5];
 const FaceLandmarker = () => {
   const faceLandmarkerRef = useRef(false);
   const pipelineRef = useRef(0);
@@ -100,7 +101,7 @@ const FaceLandmarker = () => {
   const [rejectMessage, setRejectMessage] = useState('');
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => { 
+  useEffect(() => {
     const pipelineQueryParam = new URL(window.location.href).searchParams.get('pipeline');
     const messagesQueryParam = new URL(window.location.href).searchParams.get('messages');
 
@@ -125,7 +126,7 @@ const FaceLandmarker = () => {
       });
       faceLandmarkerRef.current = newFaceLandmarker;
       enableCam();
-    };   
+    };
     createFaceLandmarker();
   }, []);
 
@@ -189,15 +190,15 @@ const FaceLandmarker = () => {
       const obj = {
         image: base64
       };
-      
+
       const idRequest = new URL(window.location.href).searchParams.get('id_request');
       const app = new URL(window.location.href).searchParams.get('app');
-      if(isLast){
+      if (isLast) {
         obj.transactionId = "EKYC_" + ((new Date()).getTime()) + "_bmsk";
         obj.idRequest = idRequest;
         obj.app = app;
       }
-      if(idRequest && app && app !== '' && idRequest !== ''){
+      if (idRequest && app && app !== '' && idRequest !== '') {
         setRejectMessage("")
         handleApi(obj).then((res) => {
           window.requestAnimationFrame(predictWebcam);
@@ -244,18 +245,15 @@ const FaceLandmarker = () => {
         (shape) => shape.categoryName === "eyeBlinkLeft"
       )?.score;
 
-
       const currentTask = dynamicPipelineRef.current[pipelineRef.current]?.task;
       if (eyelookinleftValue > 0.5 && currentTask === 'hadap-kiri') {
-        // pipelineFunc(pipelineRef.current, pipelineCount)
-
         isLoadingRef.current = true;
         storeData(pipelineRef.current === pipelineCount).then((res) => {
           if (pipelineRef.current === pipelineCount) {
             cameraRef.current.getTracks().forEach(track => track.stop());
             // handleLiveness(res.image)
             // .then(() => {
-              window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id='+ res.transactionId;
+            window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
             // })
           } else {
             setPipelineIndex((val) => {
@@ -272,7 +270,7 @@ const FaceLandmarker = () => {
             cameraRef.current.getTracks().forEach(track => track.stop());
             // handleLiveness(res.image)
             // .then(() => {
-              window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id='+ res.transactionId;
+            window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
             // })
           } else {
             setPipelineIndex((val) => {
@@ -288,7 +286,7 @@ const FaceLandmarker = () => {
             cameraRef.current.getTracks().forEach(track => track.stop());
             // handleLiveness(res.image)
             // .then(() => {
-              window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id='+ res.transactionId;
+            window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
             // })
           } else {
             setPipelineIndex((val) => {
@@ -300,13 +298,12 @@ const FaceLandmarker = () => {
       } else if (eyeBlink > 0.65 && currentTask === 'kedip-mata') {
         isLoadingRef.current = true;
         setTimeout(() => {
-
           storeData(pipelineRef.current === pipelineCount).then((res) => {
             if (pipelineRef.current === pipelineCount) {
               cameraRef.current.getTracks().forEach(track => track.stop());
               // handleLiveness(res.image)
               // .then(() => {
-                window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id='+ res.transactionId;
+              window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
               // })
             } else {
               setPipelineIndex((val) => {
@@ -324,7 +321,7 @@ const FaceLandmarker = () => {
               cameraRef.current.getTracks().forEach(track => track.stop());
               // handleLiveness(res.image)
               // .then(() => {
-                window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id='+ res.transactionId;
+              window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
               // })
             } else {
               setPipelineIndex((val) => {
@@ -334,6 +331,34 @@ const FaceLandmarker = () => {
             }
           }).catch(() => { });
         }, 1000)
+      } else if (jawopenValue < 0.2 && currentTask === 'face-liveness') {
+        isLoadingRef.current = true;
+        storeData(pipelineRef.current === pipelineCount).then((res) => {
+          if (pipelineRef.current === pipelineCount) {
+            cameraRef.current.getTracks().forEach(track => track.stop());
+            handleLiveness(res.image)
+              .then((res) => {
+                console.log(res)
+              })
+            window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
+          } else {
+            handleLiveness(res.image)
+              .then((res) => {
+                alert('Please Try Again, your face detected as' + res.message.results[0].liveness)
+
+                // if (res.message.results[0].liveness === 'real') {
+                //   alert('Please Try Again, your face detected as' + res.message.results[0].liveness)
+                //   // setPipelineIndex((val) => {
+                //   //   pipelineRef.current = val + 1;
+                //   //   return val + 1
+                //   // })
+                // } else {
+                //   alert('Please Try Again, your face detected as' + res.message.results[0].liveness)
+                // }
+              })
+
+          }
+        }).catch(() => { });
       }
     }
 
@@ -371,21 +396,75 @@ const FaceLandmarker = () => {
     return text;
   }, [messageError]);
 
+  const handleCapture = async () => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+
+    ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+
+    const imageData = canvas.toDataURL('image/jpeg');
+    const base64Response = await fetch(imageData);
+    const blob = await base64Response.blob();
+
+    const file = new File([blob], 'captured_image.jpeg', { type: 'image/jpeg' });
+
+    setCapturedImage(imageData);
+
+    const handleOCR = async () => {
+      try {
+        const token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ2Qk55YVpaU0dBVk5Zek12ZEp2ajhWUkdyOFVGUF9qUnh1dFdFd3Exa0RZIn0.eyJleHAiOi0xODAxNTkxNTU4LCJpYXQiOjE2MjkzNzU3MzgsImp0aSI6IjExYWVjZjJlLTNhNDMtNDEyMy05MDFjLTZkOGI0YjliMWMwOSIsImlzcyI6Imh0dHA6Ly9rZXljbG9hazo4MDgwL2F1dGgvcmVhbG1zL3BpY2Fzby1wbGF0Zm9ybSIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiIwYjU1OTNhMi03MTQ4LTRkNzAtOTBkMC0yMTI3NGQyMjdmMDEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJhZG1pbiIsInNlc3Npb25fc3RhdGUiOiI4OTRhYmE4OS1hYTFjLTQwNDEtYmIyZC0yNGQ2YTEwMDQ2NDAiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vZHNjLW9jci51ZGF0YS5pZDo4MDgzIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiYWJkYW5tdWxpYTQgYWJkYW5tdWxpYTQiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJjZDMyN2U3ZS1kNDQ0LTRkZGMtOTMyZS04NGYyYjBhOTMyY2EiLCJnaXZlbl9uYW1lIjoiYWJkYW5tdWxpYTQiLCJmYW1pbHlfbmFtZSI6ImFiZGFubXVsaWE0IiwiZW1haWwiOiJqc3Vwb3lvQGdtYWlsLmNvbSJ9.QHe4RwUVmRhE8DunHEte5DSgJfjfJ7MjDPkQUsOVNFUW600bAmAssAsWSCDNogUw__161jv6LzzBaqa0dTNEhZOmfl3wVoRK7Km1ZJsnSmcm6y2y05WbKKChvdbDTGw8zyCmt5iFOtnZLh1Y-U2M1EvogjzFTLHGf_FPPAHtGRXR9w2GOOiXjvCCLq9Nng7rtVyLj0vRAQG4KThkjm0mCIsWyUBnl96lmicARsedEhOH44DyrlyoXs5rA8BKbgXJuMKAorI36I3U-4C9IbBKfYQeZg0lo5Z-V4tbPVgNYvTnSK9lNCR3Su8polqTt8dFgg8QIIf-kv7bDtJ42EEJrA';
+        const formData = new FormData();
+        formData.append('image', file);
+
+        const requestOptions = {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${token}`
+          },
+          body: formData
+        };
+
+        const response = await fetch('https://bigvision.id/upload/ktp-extraction  ', requestOptions);
+        const data = await response.json();
+
+        console.log('Response from server:', data);
+        alert(
+          'Nama: ' + data.message.results.nama + '\n' +
+          'NIK: ' + data.message.results.nik + '\n' +
+          'Tanggal Lahir: ' + data.message.results.ttl
+        );
+        setPipelineIndex((val) => {
+          pipelineRef.current = val + 1;
+          return val + 1
+        })
+      } catch (error) {
+        console.error('Error sending image:', error);
+      }
+    };
+
+    handleOCR();
+  };
+
   return (
     <div>
       <section id="demos">
         <div id="liveView" className="videoView">
-          <img className="bg-image" alt="" src={require('../assets/Subtract.png')} />
+          {(dynamicPipeline[pipelineIndex]?.task) !== 'ktp-extract' &&
+            <img className="bg-image" alt="" src={require('../assets/Subtract.png')} />
+          }
           <div style={{ position: 'relative' }}>
             {videoRef &&
-              <video  poster="noposter" ref={videoRef} style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100vh', objectFit: 'cover', overflow: 'hidden' }} autoPlay playsInline></video>
+              <video poster="noposter" ref={videoRef} style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100vh', objectFit: 'cover', overflow: 'hidden' }} autoPlay playsInline></video>
             }
             {
               rejectMessage !== '' ? (
-                <div style={{ zIndex:"2000", background:"#C40C0C", position: 'absolute', top: 0, left: 0, right: 0, height: 'fit'}}>
+                <div style={{ zIndex: "2000", background: "#C40C0C", position: 'absolute', top: 0, left: 0, right: 0, height: 'fit' }}>
                   <span style={{ color: "white", padding: "10px", display: "block" }}>{rejectMessage}</span>
                 </div>
-              ): null
+              ) : null
             }
             {loading ?
               <div style={{ position: 'fixed', fontSize: 26, fontWeight: 600, top: 50, left: 0, right: 0, zIndex: 1000 }}>
@@ -404,6 +483,14 @@ const FaceLandmarker = () => {
           </div>
         </div>
         <div style={{ position: 'fixed', bottom: 70, left: 0, right: 0, zIndex: 1000 }}>
+          {/* {capturedImage && (
+            <div style={{ position: 'fixed', bottom: 10, left: 10, zIndex: 1000 }}>
+              <img src={capturedImage} alt="Captured" style={{ maxWidth: 200, maxHeight: 200 }} />
+            </div>
+          )} */}
+          {(dynamicPipeline[pipelineIndex]?.task) === 'ktp-extract' &&
+            <button onClick={handleCapture}>Capture</button>
+          }
           <span style={{ color: 'white' }}>{isLastMessage}</span>
         </div>
       </section>
