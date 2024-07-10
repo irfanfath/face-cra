eab8c4d4d4d36646fa5e22712f6efd43b764f573
irfanfath <mirfanf7@gmail.com>
2024-07-10 20:36:50 +0700

    Update OCRDemo.jsx

--- src/component/OCRDemo.jsx
+++ src/component/OCRDemo.jsx
@@ -67,7 +67,7 @@ const OCRDemo = () => {
                 ref={webcamRef}
                 screenshotFormat="image/jpeg"
                 videoConstraints={videoConstraints}
-                style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100vh', objectFit: 'cover', overflow: 'hidden' }} />
+                style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: 'auto', objectFit: 'cover', overflow: 'hidden' }} />
             <button
                 style={{ position: 'absolute' }}
                 onClick={capture}
