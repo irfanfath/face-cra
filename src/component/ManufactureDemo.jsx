b876ed75e81e1ab5bdc6d74bc4791fb9ca81fdb5
irfanfath <mirfanf7@gmail.com>
2024-08-27 14:23:39 +0700

    Update ManufactureDemo.jsx

--- src/component/ManufactureDemo.jsx
+++ src/component/ManufactureDemo.jsx
@@ -237,33 +237,19 @@ export default function ManufactureDemo() {
             //     left: 0,
             //     top: 0,
             //     width: '100%',
-            //     height: 'fit-content',
-            //     objectFit: 'contain',
+            //     height: 'auto',
+            //     objectFit: 'cover',
             //     overflow: 'hidden'
             //   }}
             // />
-            <div style={{ 
-              display: 'grid',
-              placeItems: 'center',
-              height: '100vh',
-              width: '100%',
-              background: '#000000'
-            }}>
-              <Webcam
-                className="webcam"
-                audio={false}
-                ref={webcamRef}
-                screenshotFormat="image/jpeg"
-                videoConstraints={videoConstraints}
-                screenshotQuality={1}
-                style={{
-                  background: '#000000',
-                  width: '100%',
-                  height: 'auto', 
-                  objectFit: 'cover'
-                }}
-              />
-            </div>
+            <Webcam
+              mirrored={true}
+              audio={false}
+              ref={webcamRef}
+              screenshotQuality={1}
+              screenshotFormat="image/jpeg"
+              style={{ background: '#000000', position: 'absolute', left: 0, top: 0, width: '100%', height: '100vh', objectFit: 'contain', overflow: 'hidden' }}
+            />
           }
 
           <div style={{ position: 'fixed', bottom: 30, left: 0, right: 0, zIndex: 1000 }}>
