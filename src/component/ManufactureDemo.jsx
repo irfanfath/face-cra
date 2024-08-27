fd098705e57f4119560ac0597ef38977c2ca364a
irfanfath <mirfanf7@gmail.com>
2024-08-27 16:09:45 +0700

    Update ManufactureDemo.jsx

--- src/component/ManufactureDemo.jsx
+++ src/component/ManufactureDemo.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 import Webcam from "react-webcam";
-import bgImage from '../assets/bg-camera.png';
+import bgImage from '../assets/bg-ocr.png';
 import { ArrowLeft, ArrowRight, ArrowUpDown, Camera, CircleCheck, Pencil } from "lucide-react";
 import { Editor, EditorState, ContentState, convertToRaw } from 'draft-js';
 import 'draft-js/dist/Draft.css';
