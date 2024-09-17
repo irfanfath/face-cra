034c875d07483bb012bb29a7c04e1bc1308a95e7
KhoirH <hilmi.khoirulloh02@gmail.com>
2024-09-17 17:12:02 +0700

    added front-face data

--- src/component/FaceLandmarkerDemo.jsx
+++ src/component/FaceLandmarkerDemo.jsx
@@ -55,34 +55,6 @@ const handleLiveness = (image) => {
   })
 }
 
-const handleSimilarity = (image) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ2Qk55YVpaU0dBVk5Zek12ZEp2ajhWUkdyOFVGUF9qUnh1dFdFd3Exa0RZIn0.eyJleHAiOi0xODAxNTkxNTU4LCJpYXQiOjE2MjkzNzU3MzgsImp0aSI6IjExYWVjZjJlLTNhNDMtNDEyMy05MDFjLTZkOGI0YjliMWMwOSIsImlzcyI6Imh0dHA6Ly9rZXljbG9hazo4MDgwL2F1dGgvcmVhbG1zL3BpY2Fzby1wbGF0Zm9ybSIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiIwYjU1OTNhMi03MTQ4LTRkNzAtOTBkMC0yMTI3NGQyMjdmMDEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJhZG1pbiIsInNlc3Npb25fc3RhdGUiOiI4OTRhYmE4OS1hYTFjLTQwNDEtYmIyZC0yNGQ2YTEwMDQ2NDAiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vZHNjLW9jci51ZGF0YS5pZDo4MDgzIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiYWJkYW5tdWxpYTQgYWJkYW5tdWxpYTQiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJjZDMyN2U3ZS1kNDQ0LTRkZGMtOTMyZS04NGYyYjBhOTMyY2EiLCJnaXZlbl9uYW1lIjoiYWJkYW5tdWxpYTQiLCJmYW1pbHlfbmFtZSI6ImFiZGFubXVsaWE0IiwiZW1haWwiOiJqc3Vwb3lvQGdtYWlsLmNvbSJ9.QHe4RwUVmRhE8DunHEte5DSgJfjfJ7MjDPkQUsOVNFUW600bAmAssAsWSCDNogUw__161jv6LzzBaqa0dTNEhZOmfl3wVoRK7Km1ZJsnSmcm6y2y05WbKKChvdbDTGw8zyCmt5iFOtnZLh1Y-U2M1EvogjzFTLHGf_FPPAHtGRXR9w2GOOiXjvCCLq9Nng7rtVyLj0vRAQG4KThkjm0mCIsWyUBnl96lmicARsedEhOH44DyrlyoXs5rA8BKbgXJuMKAorI36I3U-4C9IbBKfYQeZg0lo5Z-V4tbPVgNYvTnSK9lNCR3Su8polqTt8dFgg8QIIf-kv7bDtJ42EEJrA'
-      const blob = base64ToBlob(image, 'image/jpeg');
-      const fotoktp = localStorage.getItem('ktp').split(',')[1];
-      const blob2 = await fetch(`data:image/jpeg;base64,${fotoktp}`).then(res => res.blob());
-      const formData = new FormData();
-      formData.append('image_1', blob, 'image.jpg');
-      formData.append('image_2', blob2, 'image.jpg');
-
-      const action = await fetch('https://bigvision.id/upload/face-similarity', {
-        body: formData,
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        headers: {
-          "Authorization": `Bearer ${token}`,
-        },
-      })
-      const data = await action.json();
-      return resolve(data)
-    } catch (e) {
-      return reject(e)
-    }
-  })
-}
-
 
 function base64ToBlob(base64, type = 'application/octet-stream') {
   const binaryString = window.atob(base64);
@@ -107,15 +79,11 @@ const FaceLandmarker = () => {
   const cameraRef = useRef(null);
   const dynamicPipelineRef = useRef(pipeline);
   const requestAnimationRef = useRef(null);
-  const webcamRunningRef = useRef(false);
   const videoBlendShapesRef = useRef(null);
   const [pipelineIndex, setPipelineIndex] = useState(0);
   const [instructionMessage, setInstructionMessage] = useState('');
   const [rejectMessage, setRejectMessage] = useState('');
   const [loading, setLoading] = useState(true)
-  const [isLiveness, setIsLiveness] = useState(false);
-  const [dataLiveness, setDataLiveness] = useState('');
-  const [dataSimilarity, setDataSimilarity] = useState('');
   
   const isLoadFirstWasm = useRef(false);
   const isLoadFirstCamera = useRef(false);
@@ -142,6 +110,8 @@ const FaceLandmarker = () => {
   // from camera 
   useEffect(() => {
     if(!isLoadFirstPageCamera.current) {
+      console.time('cameraLoad'); // Start timing
+
       navigator.mediaDevices.getUserMedia({
         video: {
           facingMode: 'user',
@@ -155,7 +125,8 @@ const FaceLandmarker = () => {
         cameraRef.current = stream;
         videoRef.current.addEventListener("loadeddata", () => {
           isLoadFirstCamera.current = true;
-          console.log('masuk sini');
+          console.timeEnd('cameraLoad'); // end timing
+
           if(isLoadFirstWasm.current && isLoadFirstCamera.current) {
             setLoading(false);
             predictWebcam();
@@ -175,8 +146,6 @@ const FaceLandmarker = () => {
       const loadFaceLandmarker = () => {
         return new Promise(async(resolve, reject) => {
           try {
-            const startDate = new Date();
-            console.time('fileLoad'); // Start timing
             console.time('filesetResolver'); // Start timing
             // const filesetResolver = await vision.FilesetResolver.forVisionTasks(
             //   "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
@@ -198,20 +167,21 @@ const FaceLandmarker = () => {
               numFaces: 1
             });
             console.timeEnd('newFaceLandmarker'); // End timing
-            console.timeEnd('fileLoad'); // End timing
             // const endDate = new Date();
             // alert("fileload:" + (endDate.getTime() - startDate.getTime()));
-            console.log(newFaceLandmarker);
             resolve(newFaceLandmarker)
           } catch(e) {
             reject(e);
           }
         })
-      };
+      };// Start timing
 
+      console.time('fileLoad'); // Start timing
       loadFaceLandmarker().then((newFaceLandmarker) => {
         faceLandmarkerRef.current = newFaceLandmarker;
         isLoadFirstWasm.current = true;
+        
+        console.timeEnd('fileLoad'); 
         if(isLoadFirstWasm.current && isLoadFirstCamera.current) {
           setLoading(false);
           // request animation;
@@ -224,12 +194,12 @@ const FaceLandmarker = () => {
   }, [])
 
   const disableCam = () => {
-    window.cancelAnimationFrame(webcamRunningRef.current);
-    webcamRunningRef.current = false;
-    if (cameraRef.current) {
+    // if (cameraRef.current) {
       cameraRef.current.getTracks().forEach(track => track.stop());
-      videoRef.current.srcObject = null;
-    }
+      // videoRef.current.srcObject = null;
+      // window.cancelAnimationFrame(requestAnimationRef.current);
+      // requestAnimationRef.current = false;
+    // }
   };
 
   const predictWebcam = async (e) => {
@@ -301,7 +271,11 @@ const FaceLandmarker = () => {
         (shape) => shape.categoryName === "jawOpen"
       )?.score;
 
-      const eyeBlink = blendShapes[0]?.categories.find(
+      const eyeBlinkLeft = blendShapes[0]?.categories.find(
+        (shape) => shape.categoryName === "eyeBlinkLeft"
+      )?.score;
+
+      const eyeBlinkRight = blendShapes[0]?.categories.find(
         (shape) => shape.categoryName === "eyeBlinkLeft"
       )?.score;
 
@@ -312,15 +286,9 @@ const FaceLandmarker = () => {
           if (pipelineRef.current === pipelineCount) {
             disableCam()
             handleLiveness(res.image)
-              .then((res) => {
-                setDataLiveness(res.message.results[0].liveness)
+              .then(() => {
+                window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
               })
-            handleSimilarity(res.image)
-              .then((res) => {
-                setDataSimilarity(res.message.results.status)
-                setIsLiveness(true)
-              })
-            // window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
           } else {
             setPipelineIndex((val) => {
               pipelineRef.current = val + 1;
@@ -334,15 +302,9 @@ const FaceLandmarker = () => {
           if (pipelineRef.current === pipelineCount) {
             disableCam()
             handleLiveness(res.image)
-              .then((res) => {
-                setDataLiveness(res.message.results[0].liveness)
+              .then(() => {
+                window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
               })
-            handleSimilarity(res.image)
-              .then((res) => {
-                setDataSimilarity(res.message.results.status)
-                setIsLiveness(true)
-              })
-            // window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
           } else {
             setPipelineIndex((val) => {
               pipelineRef.current = val + 1;
@@ -356,15 +318,9 @@ const FaceLandmarker = () => {
           if (pipelineRef.current === pipelineCount) {
             disableCam();
             handleLiveness(res.image)
-              .then((res) => {
-                setDataLiveness(res.message.results[0].liveness)
+              .then(() => {
+                window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
               })
-            handleSimilarity(res.image)
-              .then((res) => {
-                setDataSimilarity(res.message.results.status)
-                setIsLiveness(true)
-              })
-            // window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
           } else {
             setPipelineIndex((val) => {
               pipelineRef.current = val + 1;
@@ -372,22 +328,16 @@ const FaceLandmarker = () => {
             })
           }
         }).catch(() => { });
-      } else if (eyeBlink > 0.65 && currentTask === 'kedip-mata') {
+      } else if (eyeBlinkLeft > 0.65 && eyeBlinkRight > 0.65 && currentTask === 'kedip-mata') {
         isLoadingRef.current = true;
         setTimeout(() => {
           storeData(pipelineRef.current === pipelineCount).then((res) => {
             if (pipelineRef.current === pipelineCount) {
               disableCam()
               handleLiveness(res.image)
-                .then((res) => {
-                  setDataLiveness(res.message.results[0].liveness)
+                .then(() => {
+                  window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
                 })
-              handleSimilarity(res.image)
-                .then((res) => {
-                  setDataSimilarity(res.message.results.status)
-                  setIsLiveness(true)
-                })
-              // window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
             } else {
               setPipelineIndex((val) => {
                 pipelineRef.current = val + 1;
@@ -403,15 +353,9 @@ const FaceLandmarker = () => {
             if (pipelineRef.current === pipelineCount) {
               disableCam()
               handleLiveness(res.image)
-                .then((res) => {
-                  setDataLiveness(res.message.results[0].liveness)
+                .then(() => {
+                  window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
                 })
-              handleSimilarity(res.image)
-                .then((res) => {
-                  setDataSimilarity(res.message.results.status)
-                  setIsLiveness(true)
-                })
-              // window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
             } else {
               setPipelineIndex((val) => {
                 pipelineRef.current = val + 1;
@@ -420,19 +364,14 @@ const FaceLandmarker = () => {
             }
           }).catch(() => { });
         }, 1000)
-      } else if (jawopenValue < 0.2 && currentTask === 'face-active') {
+      } else if (eyeBlinkLeft < 0.2 && eyeBlinkRight < 0.2  && currentTask === 'face-active') {
         isLoadingRef.current = true;
         storeData(pipelineRef.current === pipelineCount).then((res) => {
           if (pipelineRef.current === pipelineCount) {
             disableCam()
             handleLiveness(res.image)
-              .then((res) => {
-                setDataLiveness(res.message.results[0].liveness)
-              })
-            handleSimilarity(res.image)
-              .then((res) => {
-                setDataSimilarity(res.message.results.status)
-                setIsLiveness(true)
+              .then(() => {
+                window.location.href = 'https://bigvision.id?image=' + res.image + '&transaction_id=' + res.transactionId;
               })
           } else {
             setPipelineIndex((val) => {
@@ -448,7 +387,6 @@ const FaceLandmarker = () => {
   }, []);
 
   const drawBlendShapesRealTime = useCallback((result) => {
-    console.log('masuk sini');
     drawBlendShapes(videoBlendShapesRef.current, result.faceBlendshapes);
 
     const faceAreaHorizontal = result?.faceLandmarks?.[0]?.[4]?.x
@@ -489,33 +427,10 @@ const FaceLandmarker = () => {
     <div>
       <section id="demos">
         <div id="liveView" className="videoView">
-          {!isLiveness && <img className="bg-image" alt="" src={require('../assets/bg-face.png')} />}
+          <img className="bg-image" alt="" src={require('../assets/bg-face.png')} />
           <div style={{ position: 'relative' }}>
-            {!isLiveness ?
-              <video className='video-face' poster="noposter" ref={videoRef} style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100vh', objectFit: 'cover', overflow: 'hidden' }} autoPlay playsInline></video>
-              :
-              <div style={{ marginTop: '10vh' }}>
-                <div className="bg-welcoming" style={{ marginTop: '20px', padding: '20px', marginBottom: '5%' }}>
-                  <div className="bg-ktp-result" style={{ display: 'inline-flex', placeItems: 'center', width: '80%' }}>
-                    <CircleCheck color="#0a8053" size={50} />
-                    <div style={{ fontSize: '20px', fontWeight: '600', textAlign: 'left', marginLeft: '20px' }}>Face Recognition <br /><strong>Berhasil</strong></div>
-                  </div>
-                  <div style={{ marginTop: '50px' }}>
-                    <div style={{ fontSize: '20px' }}>Wajah yang terdeteksi adalah <br /><strong style={{ textTransform: 'capitalize' }}>{dataLiveness}</strong> Face!</div>
-                  </div>
-                  <div style={{ marginTop: '50px' }}>
-                    <div style={{ fontSize: '20px' }}>Hasil Kecocokan KTP dan wajah terdeteksi sebagai <br /><strong style={{ textTransform: 'capitalize' }}>{dataSimilarity}</strong> Face!</div>
-                  </div>
-                  <div style={{ marginTop: '50px' }}>
-                    <button className="next-button" onClick={restartStep}>Menu Utama</button>
-                  </div>
-                </div>
-                <div style={{ marginTop: '40px', marginBottom: '20px' }}>
-                  <img src={require('../assets/bigvision.png')} alt="Welcoming" />
-                </div>
-              </div>
-            }
-
+            <video className='video-face' poster="noposter" ref={videoRef} style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100vh', objectFit: 'cover', overflow: 'hidden' }} autoPlay playsInline></video>
+              
             {
               rejectMessage !== '' ? (
                 <div style={{ zIndex: "2000", background: "#C40C0C", position: 'absolute', top: 0, left: 0, right: 0, height: 'fit' }}>
@@ -525,22 +440,19 @@ const FaceLandmarker = () => {
             }
 
             {
-              !isLiveness ?
-                loading ?
-                  <div style={{ position: 'fixed', fontSize: 26, fontWeight: 600, top: 50, left: 0, right: 0, zIndex: 1000 }}>
-                    <span style={{ color: 'white' }}>Harap Tunggu<br /><span style={{ fontSize: 20 }}>sedang memproses kamera</span></span>
-                  </div>
-                  :
-                  <div style={{ position: 'fixed', fontSize: 22, fontWeight: 600, top: 50, left: 0, right: 0, zIndex: 1000 }}>
-                    <span style={{ color: 'white' }}>
-                      {(message && message[pipelineIndex]) || (dynamicPipeline[pipelineIndex]?.word)}
-                      <br />
-                      <span style={{ fontSize: 20 }}>{instructionMessage}</span>
-                    </span>
-                  </div>
+              loading ?
+                <div style={{ position: 'fixed', fontSize: 26, fontWeight: 600, top: 50, left: 0, right: 0, zIndex: 1000 }}>
+                  <span style={{ color: 'white' }}>Harap Tunggu<br /><span style={{ fontSize: 20 }}>sedang memproses kamera</span></span>
+                </div>
                 :
-                ''
-            }
+                <div style={{ position: 'fixed', fontSize: 22, fontWeight: 600, top: 50, left: 0, right: 0, zIndex: 1000 }}>
+                  <span style={{ color: 'white' }}>
+                    {(message && message[pipelineIndex]) || (dynamicPipeline[pipelineIndex]?.word)}
+                    <br />
+                    <span style={{ fontSize: 20 }}>{instructionMessage}</span>
+                  </span>
+                </div>
+            }  
           </div>
         </div>
         <div style={{ position: 'fixed', bottom: 70, left: 0, right: 0, zIndex: 1000 }}>
